5e6c5e7ea1335f8a503a9ab4508fd6c5eae6fd38
smv5047 <smv5047@gmail.com>
2020-07-12 19:38:01 -0400

    searching completed

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,7 +1,8 @@
 def linear_search(arr, target):
     # Your code here
-
-
+    for i in range(0, len(arr)):
+        if arr[i] == target:
+            return i
     return -1   # not found
 
 
@@ -9,6 +10,23 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
-
-
+    new_arr = arr
+    low = 0
+    high = len(arr)-1
+    while low <= high:
+      # determine midpoint
+        midpoint = (low+high)//2
+        if target < arr[midpoint]:
+            high = midpoint-1
+        elif target > arr[midpoint]:
+            low = midpoint+1
+        else:
+            return midpoint
+        # if equal return
     return -1  # not found
+
+
+test_array = [1, 14, 17, 18, 19, 20, 24]
+
+print(linear_search(test_array, 4))
+print(binary_search(test_array, 20))
